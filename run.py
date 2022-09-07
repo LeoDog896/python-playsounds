cd76ea9a6a7ecbcc71a3da6014dabd671cdcbcf0
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-09-07 17:46:08 -0400

    Update run.py

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 while True:
   rand = random.randint(0, len(onlyfiles))
   print(rand)
+  print(onlyfiles)
   os.system("cvlc --gain 1000 --vout none --play-and-exit /home/pi/sound/" + str(rand))
   sleep(1)
  #sleep(random.randint(30,3600))
