9335eaf51c14d5756972405509065d850e77ef43
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-09-07 17:55:51 -0400

    start up sound

--- run.py
+++ run.py
@@ -9,10 +9,14 @@ mypath = '/home/pi/python-playsounds/sounds/'
 
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
+os.system("cvlc --gain 100 --vout none --play-and-exit " +  mypath + "Treasure_Chest_Long_1.wav")
+
 while True:
   rand = random.randint(0, len(onlyfiles) - 1)
-  print(rand)
-  print(onlyfiles)
+  sleep(random.randint(30,3600))
+  
+  #print(rand)
+  #print(onlyfiles)
   os.system("cvlc --gain 100 --vout none --play-and-exit " +  mypath + onlyfiles[rand])
   sleep(1)
- #sleep(random.randint(30,3600))
+  #sleep(random.randint(30,3600))
