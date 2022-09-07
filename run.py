f5ca2b44dd06dbabeb0c712950bbceb57772feda
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-09-07 17:43:36 -0400

    add more sounds

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 import os
 import random
 from time import sleep
+from os import listdir
+import pathlib
+from os.path import isfile, join
+
+mypath = '/home/pi/python-playsounds/'
+
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 while True:
-  rand = random.randint(0,30)
+  rand = random.randint(0, len(onlyfiles))
   print(rand)
-  os.system("cvlc --gain 1000 --vout none --play-and-exit /home/pi/sound/" + str(rand) + ".ogg")
- #sleep(1)
-  sleep(random.randint(30,3600))
+  os.system("cvlc --gain 1000 --vout none --play-and-exit /home/pi/sound/" + str(rand))
+  sleep(1)
+ #sleep(random.randint(30,3600))
