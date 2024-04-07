7c7905e0312b572800e866ea4a620149ac5df2c0
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-04-07 18:56:11 -0400

    feat: add initial log to py

--- run.py
+++ run.py
@@ -11,6 +11,10 @@ files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
 os.system("aplay " +  my_path + "Treasure_Chest_Long.wav")
 
 print("Playing sounds every 30-60 minutes")
+print("Sound directory: " + my_path)
+print("Sounds available: " + str(files))
+print("Ctrl + C to stop")
+
 lower = 60 * 30
 upper = 60 * 60
 
