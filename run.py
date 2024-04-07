7568f7df46ccefd4204568ad8679429690125c3e
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-04-07 18:55:48 -0400

    refactor: clean up and log

--- run.py
+++ run.py
@@ -2,24 +2,25 @@ import os
 import random
 from time import sleep
 from os import listdir
-import pathlib
 from os.path import isfile, join
 
-mypath = '/home/pi/python-playsounds/sounds/'
+my_path = os.path.dirname(os.path.realpath(__file__)) + "/sounds/"
 
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
 
-os.system("aplay " +  mypath + "Treasure_Chest_Long.wav")
+os.system("aplay " +  my_path + "Treasure_Chest_Long.wav")
+
+print("Playing sounds every 30-60 minutes")
+lower = 60 * 30
+upper = 60 * 60
 
 while True:
-  # wait so the sounds are not distracting
-  sleep(1)
-  random_Time = random.randint(1800,3600)
-  #print(random_Time)
-  sleep(random_Time)
+  time = random.randint(lower, upper)
+  print("Sleeping for " + str(time) + " seconds")
+  sleep(time)
 
   # now play the sounds
-  rand = random.randint(0, len(onlyfiles) - 1)
-  #print(rand)
-  #print(onlyfiles)
-  os.system("aplay " +  mypath + onlyfiles[rand])
+  rand = random.randint(0, len(files) - 1)
+  file = files[rand]
+  print("Playing " + file)
+  os.system("aplay " +  my_path + files[rand])
